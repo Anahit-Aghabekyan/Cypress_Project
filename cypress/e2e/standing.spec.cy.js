a1b13169c25265e14c18d6ae544212fa3d288ff9
Anahit-Aghabekyan <anahit.aghabekyann@gmail.com>
2023-04-03 12:37:30 -0400

    Making some changes

--- cypress/e2e/standing.spec.cy.js
+++ cypress/e2e/standing.spec.cy.js
@@ -1,12 +1,15 @@
-describe('template spec', () => {
-    it('Go to WNBA Web page', () => {
+describe('e2e automation spec for Cypress', () => {
+
+    it('should go to WNBA Web page', () => {
     cy.visit('https://www.wnba.com/standings/#?season=2022')
     })
-    it('Select 2018 Season', () => {
+
+    it('should select 2018 Season', () => {
         cy.wait(2000)
     cy.get('#filter-season').select('2018 Season')
     })
-    it('Verify number of row in the table is 12', () => {
+
+    it('should verify number of row in the table is 12', () => {
     let numberOfTeam
     cy.get('#league > wnba-stat-table >.row > .stat-table__overflow >.table > tbody')
     .find("tr")
@@ -15,7 +18,8 @@ describe('template spec', () => {
     expect (numberOfTeam).to.equal(12)
     });
     })
-    it('Print team names in column 2', () => {
+    
+    it('should print team names in column 2', () => {
     cy.get('#league > wnba-stat-table > .row >.stat-table__overflow > .table > tbody')
     .find("tr")
     .find("th").each(($el, index, $list)=>{
